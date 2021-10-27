2859232c0ed228665d3b221fd4aa5d780810f153
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2021-10-26 22:01:50 -0500

    chore: probando el servicio con primer nombre y primer apellido

---
+++ src/main/java/com/nttdata/examen/web/rest/CobGestorController.java
@@ -0,0 +1,23 @@
+package com.nttdata.examen.web.rest;
+
+import com.nttdata.examen.model.CobGestor;
+import com.nttdata.examen.repository.CobGestorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CobGestorController {
+
+    @Autowired
+    private CobGestorRepository cobGestorRepository;
+
+    @GetMapping("/carlos")
+    public List<String> listar(){
+        return cobGestorRepository.findByConcatNames();
+    }
+}
